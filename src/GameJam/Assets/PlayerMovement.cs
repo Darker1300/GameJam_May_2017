28e3983950c88d3170bb1a3366276f7f829615fc
David.Floyd <david.floyd1988@gmail.com>
2017-05-24 15:24:37 +1000

    Prototype push

---
+++ src/GameJam/Assets/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public GameObject Player;
+
+    public float playerMoveSpeed;
+    static float moveSpeedModifier; 
+
+	// Use this for initialization
+	void Start () {
+        Player = this.gameObject;
+        playerMoveSpeed = 10;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Player.transform.Translate(-Vector3.right * playerMoveSpeed * Time.deltaTime, Space.World);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Player.transform.Translate(Vector3.right * playerMoveSpeed * Time.deltaTime, Space.World);
+
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Player.transform.Translate(Vector3.forward * playerMoveSpeed * Time.deltaTime, Space.World);
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Player.transform.Translate(-Vector3.forward * playerMoveSpeed * Time.deltaTime, Space.World);
+
+        }
+
+    }
+}
---
+++ src/GameJam/Assets/RotateBelt.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBelt : MonoBehaviour {
+
+    public float RotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+        RotateSpeed = 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.gameObject.transform.Rotate(0, RotateSpeed, 0);
+	}
+}
