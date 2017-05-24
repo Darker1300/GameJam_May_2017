1320dc491857c4f87c6637cdd9067c509e943df6
Ethan Moffatt <darker1300@gmail.com>
2017-05-24 17:35:34 +1000

    Starting Spawning.

---
+++ src/GameJam/Assets/Scripts/Testbed/ItemManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour {
+
+    [Header("Spawnable Items")]
+    public List<GameObject> items = new List<GameObject>();
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
