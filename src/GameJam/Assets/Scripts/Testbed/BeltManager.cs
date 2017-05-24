8265c92a74c5f3929ce7cb2a6a17a0abe2155a74
Ethan Moffatt <darker1300@gmail.com>
2017-05-24 16:36:32 +1000

    Setting up some prefabs and scripts.

---
+++ src/GameJam/Assets/Scripts/Testbed/BeltManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BeltManager : MonoBehaviour
+{
+    [Header("Transform")]
+    public Transform BeltTopTransform = null;
+    public float BeltRadius = 8.5f;
+    public int BeltCapacity = 10;
+    public float BeltOffset = 0.0f;
+
+    void Start()
+    {
+        if (!BeltTopTransform) BeltTopTransform = this.transform;
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (!BeltTopTransform) BeltTopTransform = this.transform;
+        Gizmos.DrawWireSphere(BeltTopTransform.position, BeltRadius);
+        float interval = 360.0f / BeltCapacity;
+        for (int i = 0; i < BeltCapacity; i++)
+            Gizmos.DrawWireSphere(DegreesToPosition(IndexToDegrees(i)), 0.5f);
+    }
+
+    float IndexToDegrees(int i)
+    {
+        return (360.0f / BeltCapacity * i) + BeltOffset;
+    }
+
+    Vector3 DegreesToPosition(float _degrees)
+    {
+        Vector3 pos = BeltTopTransform.position;
+        pos.x += BeltRadius * Mathf.Sin(_degrees * Mathf.Deg2Rad);
+        pos.z += BeltRadius * Mathf.Cos(_degrees * Mathf.Deg2Rad);
+        return pos;
+    }
+}
---
+++ src/GameJam/Assets/Scripts/Testbed/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+    public float spawnRatio = 0.1f;
+    public float pointValue = 1.0f;
+    public ItemType itemType = ItemType.Standard;
+    public float speedModifier = 0.0f;
+    public float speedTime = 1.0f;
+
+    public enum ItemType
+    {
+        Standard
+    }
+}
